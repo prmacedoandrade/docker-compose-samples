48d59454ce9e1f4756499307a68f7f0f975cf6ef
Paulo Renato Macêdo <prmacedoandrade@gmail.com>
2024-07-15 18:05:11 -0300

    Adding more commands

--- aws-cli-docker/scripts-s2.sh
+++ aws-cli-docker/scripts-s2.sh
@@ -8,3 +8,17 @@ aws ec2 describe-instances --filters "Name=tag:group,Values=aws-cli" --region us
 aws ec2 describe-instances --filters "Name=tag:group,Values=aws-cli" --query "Reservations[*].Instances[*].[InstanceId,ImageId,Tags[?Key=='group'].Value[]]" --region us-east-1
 aws ec2 describe-instances --filters "Name=tag:group,Values=aws-cli" --query "Reservations[*].Instances[*].[InstanceId,ImageId,Tags[?Key=='group'].Value[]]" --region us-east-1 --output table
 aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" "Name=tag:group,Values=aws-cli"  --query "Reservations[*].Instances[*].[InstanceId,ImageId,Tags[?Key=='group'].Value[]]" --region us-east-1
+
+#Stop Instances
+aws ec2 stop-instances --instance-ids 
+aws ec2 stop-instances --instance-ids i-02f6df34a5c0c0915 --output table --region us-east-1
+
+#Need to format output use tr to stop multiple instances
+instance_ids=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" "Name=tag:group,Values=aws-cli"  --query "Reservations[*].Instances[*].[InstanceId]" --region us-east-1 | tr -d [ | tr -d ] | tr -d , | tr -d '"')
+aws ec2 stop-instances --instance-ids $instance_ids --output table --region us-east-1
+
+#Start instances
+aws ec2 start-instances --instance-ids $instance_ids --output table --region us-east-1
+
+#Script terminate
+aws ec2 terminate-instances --instance-ids $instance_ids --output table --region us-east-1
